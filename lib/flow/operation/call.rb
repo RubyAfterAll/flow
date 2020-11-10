163f55f73660142927489d84d691a1b1e4efe9d5
Tom Conroy <tbconroy@gmail.com>
2020-11-10 13:39:29 -0500

    Fix syntax error

--- lib/flow/operation/call.rb
+++ lib/flow/operation/call.rb
@@ -25,7 +25,7 @@ module Flow
         def introspected_state_class
           Class.new(StateBase).tap do |state_class|
             [*_state_readers, *_state_accessors].uniq.each do |method_name|
-              state_class.__send__(:option, method_name) unless .include?(method_name)
+              state_class.__send__(:option, method_name)
             end
             _state_writers.each do |method_name|
               state_class.__send__(:output, method_name) unless _state_accessors.include?(method_name)
