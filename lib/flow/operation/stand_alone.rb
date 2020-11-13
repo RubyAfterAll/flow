206a2c7f1aa36550d689da08f055fa0d84815467
Tom Conroy <tbconroy@gmail.com>
2020-11-13 16:44:08 -0500

    Operation._state_accesors should be State.options and State.outputs

--- lib/flow/operation/stand_alone.rb
+++ lib/flow/operation/stand_alone.rb
@@ -18,24 +18,16 @@ module Flow
         private
 
         def introspected_state(kwargs)
-          # introspected_state_class instance should never be invalid, but needs to be validated for any defined outputs
-          introspected_state_class.new(kwargs).tap { |state| state.valid? }
+          introspected_state_class.new(kwargs).tap(&validate)
         end
 
         def introspected_state_class
           Class.new(StateBase).tap do |state_class|
-            introspected_state_options.each { |method_name| state_class.__send__(:option, method_name) }
-            introspected_state_outputs.each { |method_name| state_class.__send__(:output, method_name) }
+            _state_readers.each { |method_name| state_class.__send__(:option, method_name) }
+            _state_writers.each { |method_name| state_class.__send__(:output, method_name) }
           end
         end
-
-        def introspected_state_options
-          [*_state_readers, *_state_accessors].uniq
-        end
-
-        def introspected_state_outputs
-          _state_writers - _state_accessors
-        end
+        memoize :introspected_state_class
       end
     end
   end
